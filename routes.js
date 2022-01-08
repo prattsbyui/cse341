ceb416759f39ce73ea40775362a4d4c3af8c4b3d
prattsbyui <pratts@byui.edu>
2022-01-08 11:52:05 -0600

    week1

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const requestHandler= (req, res) =>{
     const url = req.url;
     const method = req.method;
+    
 if (url === '/') {
     res.write('<html>');
     res.write('<head><title>asignment 1</title><head>');
