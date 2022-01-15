051a4c80ef0180387ea1a30e6296912c8aac01c1
prattsbyui <pratts@byui.edu>
2022-01-15 14:08:46 -0600

    week2

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-const http = require('http');
+const path = require('path');
 
-const routes = require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-console.log(routes.someText);
+const app = express();
 
-const server = http.createServer(routes.handler);
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
-server.listen(3000);
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
